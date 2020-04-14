f4ef1f3d153b2e0b9a70c99d62e0ef772a1f6d0f
Geof Crowl <rectangular@gmail.com>
2020-04-14 12:59:12 -0600

    Update install.sh to handle submodules

--- install.sh
+++ install.sh
@@ -32,13 +32,9 @@ for f in .???*; do
     echo "Creating symlink $d/$f -> ~/$f"
 done
 
-echo "Installing Vundle"
+echo "Installing submodules"
 
-if [[ ! -d ~/.vim/bundle/Vundle.vim ]]; then
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-else
-    echo "Vundle install already found"
-fi
+git submodule init
 
 echo "Installing vim plugins"
 
